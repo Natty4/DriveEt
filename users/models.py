7e5e053033159d14cba1852040948d48ce886b2d
natty4 <natty7kt@gmail.com>
2026-02-11 23:03:24 +0300

    Add created_at and updated_at fields to Subscription model; create migration for database schema update

--- users/models.py
+++ users/models.py
@@ -105,6 +105,9 @@ class Subscription(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     expiry_date = models.DateTimeField()  # Computed on save
     is_active = models.BooleanField(default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
         
     class Meta:
