76830e7c196a891d4527f8543314e950a6070859
natty4 <natty7kt@gmail.com>
2026-03-13 00:07:34 +0300

    Debuging user auth

--- users/authentication.py
+++ users/authentication.py
@@ -124,8 +124,8 @@ class TelegramAuthenticationBackend(authentication.BaseAuthentication):
             
             # Extract user data
             user_json = parsed_data.get('user')
-            logger.error("user json: ", user_json)
-            logger.error("user hash data: ", init_data)
+            logger.error(f"user json: {user_json}")
+            logger.error(f"user hash data: {init_data}")
             if not user_json:
                 logger.error("No user data in init_data")
                 return None
